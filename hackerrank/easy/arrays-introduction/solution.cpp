3af430349088685d3791b8f929763d21c8f40f7e
sumit midya <sumitmidya09@gmail.com>
2026-09-11 21:32:02 +0530

    ✅ Arrays Introduction | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-11

--- hackerrank/easy/arrays-introduction/solution.cpp
+++ hackerrank/easy/arrays-introduction/solution.cpp
@@ -1,24 +1,21 @@
-#include <stdio.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
-void update(int *a,int *b) {
-    // Complete this function   
-    int c,d;
-    c=*a+*b;
-    
-    d=*a-*b;
-    *a=c;
-    if (d<0) d=-d;
-
-    *b=d;
-}
 
 int main() {
-    int a, b;
-    int *pa = &a, *pb = &b;
-    
-    scanf("%d %d", &a, &b);
-    update(pa, pb);
-    printf("%d\n%d", a, b);
-
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+     int n;
+     cin>>n;
+     int arr[n];
+     for (int i=0;i<n;i++){
+        cin>>arr[i];     
+     }
+     for (int j=n-1;j>=0;j--){
+        cout<<arr[j]<<" ";
+     }
     return 0;
 }
