987185d58b4a5971cd301cddc19cab80ac7a048b
sumit midya <sumitmidya09@gmail.com>
2026-09-11 21:19:53 +0530

    ✅ Pointer | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ hackerrank/easy/arrays-introduction/solution.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function   
+    int c,d;
+    c=*a+*b;
+    
+    d=*a-*b;
+    *a=c;
+    if (d<0) d=-d;
+
+    *b=d;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
